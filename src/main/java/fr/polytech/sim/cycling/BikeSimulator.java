77f7c477a0dae10e6aacdbe1307d72529cafe04b
Locolm <clement.kl34@gmail.com>
2024-04-10 14:17:58 +0200

    ex7 : end (WIP)

--- src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
+++ src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
@@ -4,13 +4,14 @@ import fr.polytech.sim.Simulation;
 import fr.polytech.sim.log.FileLogger;
 import fr.polytech.sim.log.Logger;
 import fr.polytech.sim.log.LoggerFactory;
+import fr.polytech.sim.log.TimestampedLoggerDecorator;
 import fr.polytech.sim.utils.Context;
 
 /**
  * Bike simulation.
  */
 public class BikeSimulator implements Simulation {
-    private final Logger logger = LoggerFactory.createLogger("BikeSimulator");
+    private final TimestampedLoggerDecorator logger = new TimestampedLoggerDecorator(LoggerFactory.createLogger("BikeSimulator"));
 
     public void run() {
         Bike bike = Context.inject(SimpleBike.class);
